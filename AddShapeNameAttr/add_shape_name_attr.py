d50fd9993dd97e481906ba604d714ed45b6caaf9
don.morrison.2000 <don.morrison.2000@gmail.com>
2018-10-04 17:25:32 -0500

    Updated logging

--- AddShapeNameAttr/add_shape_name_attr.py
+++ AddShapeNameAttr/add_shape_name_attr.py
@@ -1,14 +1,19 @@
 # To run from Spyder iPython console:
-#   runfile('D:/CRTI/python_projects/ArcGIS-scripts/AddShapeNameAttr/add_shape_name_attr.py', wdir='D:/CRTI/python_projects/ArcGIS-scripts/AddShapeNameAttr', args="")
-
+#   runfile('D:/CRTI/python_projects/ArcGIS-scripts/AddShapeNameAttr/add_shape_name_attr.py', wdir='D:/CRTI/python_projects/ArcGIS-scripts/AddShapeNameAttr', args="'D:/Temp/shp_test' 'NAME'")
 
 import arcpy
 import glob
 
-#__SHPfolder = r'\\FILER01\se\Action\GEODATA\NorthAmerica\US-Midwest\Illinois\Will_County\Will_TreeCrowns\Trees_WillCounty\Sample_tree_crowns'
-__SHPfolder = r'D:\Temp\shp_test'
-__attr = "NAME"
+# Get the input arguments
+__SHPfolder = arcpy.GetParameterAsText(0)
+__attr = arcpy.GetParameterAsText(1)
 
+def log (message):
+    print (message)
+    arcpy.AddMessage(message)
+    
+log (__SHPfolder)
+log (__attr)
 
 def add_attr ():
     # Get a list of all shape files in the specified folder
@@ -16,15 +21,15 @@ def add_attr ():
     
     # Add a new attribute to all of the shape files
     for shp in shps:
-        print('Adding ' + __attr + ' attribute to ' + shp)
+        log('Adding ' + __attr + ' attribute to ' + shp)
         arcpy.AddField_management(shp, __attr, "TEXT")
     
     # Populate the attribute in all of the shape files
     for shp in shps:
         name = shp.split('\\')[-1]
         name = name[:len(name)-4]
-        print('Setting ' + __attr + ' attribute to ' + name)
-        with arcpy.da.UpdateCursor(shp, [__attr]) as cursor: #field that will contain the Shapefile name
+        log('Setting ' + __attr + ' attribute to ' + name)
+        with arcpy.da.UpdateCursor(shp, [__attr]) as cursor: 
             for row in cursor:
                 row[0] = name
                 cursor.updateRow(row)
