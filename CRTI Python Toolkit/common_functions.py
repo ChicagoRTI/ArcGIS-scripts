e17cc4e92606bf78ff6e0b578b0a016ffc4b36e7
don.morrison.2000 <don.morrison.2000@gmail.com>
2018-11-05 10:04:22 -0600

    add gc info

--- CRTI Python Toolkit/common_functions.py
+++ CRTI Python Toolkit/common_functions.py
@@ -77,8 +77,8 @@ def step_header (step_count, step_total, message, inputs, outputs):
         log('Input : ' + m)
     for m in outputs:
         log('Output: ' + m)
+    log('GC: uncollectable=' + str(len(gc.garbage)) + ' objects=' + str(len(gc.get_objects())))
     log('--------------------------------------------------')
-    gc.collect()
 
 
 def isOptimizable (fn):
