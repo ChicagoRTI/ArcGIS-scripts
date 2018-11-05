d3b074218b02827930e13e94b041b26f7cd04390
don.morrison.2000 <don.morrison.2000@gmail.com>
2018-11-05 13:15:08 -0600

    gc change

--- CRTI Python Toolkit/common_functions.py
+++ CRTI Python Toolkit/common_functions.py
@@ -77,7 +77,9 @@ def step_header (step_count, step_total, message, inputs, outputs):
         log('Input : ' + m)
     for m in outputs:
         log('Output: ' + m)
-    log('GC: uncollectable=' + str(len(gc.garbage)) + ' objects=' + str(len(gc.get_objects())))
+    log('GC before: uncollectable=' + str(len(gc.garbage)) + ' objects=' + str(len(gc.get_objects())))
+    gc.collect()
+    log('GC after : uncollectable=' + str(len(gc.garbage)) + ' objects=' + str(len(gc.get_objects())))
     log('--------------------------------------------------')
 
 
