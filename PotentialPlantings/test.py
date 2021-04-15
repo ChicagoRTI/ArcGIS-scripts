d3aae49526aa012dae02bbe3ed1bcad508944fe9
don-morrison-2000 <don.morrison.2000@gmail.com>
2021-04-15 16:57:25 -0500

    optimizations

--- PotentialPlantings/test.py
+++ PotentialPlantings/test.py
@@ -18,14 +18,16 @@ neg_buffer = - (10 * 0.3048)
 
  
 
-OPEN = -1
+
 SMALL = 0
 MEDIUM = 1
 BIG = 2
-BEYOND_EXTENT = 100
+VACANT = 100
 OUTSIDE_POLYGON = 101
 CANOPY = 102
 
+
+TREE_SIZES = [BIG, MEDIUM, SMALL]
 
 def run ():
                                    
@@ -36,7 +38,6 @@ def run ():
     arcpy.management.Delete(neg_buffered_fc)
     arcpy.GraphicBuffer_analysis(openings_fc, neg_buffered_fc, neg_buffer)
     
-    points_checked = 0
     
     print ("Calculating points")    
     with arcpy.da.SearchCursor(neg_buffered_fc, ['OBJECTID', 'SHAPE@']) as cursor:
@@ -53,35 +54,23 @@ def run ():
             center_row = tiers
             center_col = tiers
             
-            # The mask orgin is the NW corner and indexed as  [row][col]
+            # The mask orgin is the NW corner and indexed row major as  [row][col]
             mask_dim = tiers*2 + 1
-            mask = np.full( (mask_dim, mask_dim), OPEN, dtype=int)
-            __trim_mask (mask, mask_dim, nw_corner, polygon)
-                         
-            plant_heres = list()
-            
-            
-            for tree_size in [BIG, MEDIUM, SMALL]:
-            
-                for tier in range(0, tiers+1):
-                    top  = [(center_row - tier, col)  for col in range (center_row - tier, center_row + tier + 1)]
-                    right= [(row, center_col + tier)  for row in range (center_col - tier, center_col + tier + 1)]
-                    bot  = [(center_row + tier, col)  for col in reversed(range (center_row - tier, center_row + tier + 1))]
-                    left = [(row, center_col - tier)  for row in reversed(range (center_col - tier, center_col + tier + 1))]
-#                    for row, col in top + right + bot + left:
-                    for row, col in [(r,c) for (r,c)in top + right + bot + left if mask[r, c] == OPEN]:
-#                        if mask[row, col] == OPEN:                                                  
-        
-                        fp_row, fp_col, fp_row_dim, fp_col_dim = __get_footprint (row, col, tree_size, mask_dim)
-#                       print ("(%i,%i): %i x %i" % (fp_row, fp_col, fp_row_dim, fp_col_dim))                        
-                        if __is_footprint_clean (mask, fp_row, fp_col, fp_row_dim, fp_col_dim):                        
-                            __occupy_footprint (mask, fp_row, fp_col, fp_row_dim, fp_col_dim, row, col, tree_size)
-                            plant_heres.append ( (__mask_to_point (row, col, nw_corner), tree_size) )  
+            mask = np.empty( (mask_dim, mask_dim), dtype=int)
+            points = __trim_mask_and_get_points (nw_corner, polygon, mask)
+
+            for tree_size in TREE_SIZES: 
+                for tier_idx in range(0, tiers+1):
+                    for row, col in __get_tier_vacancies (center_row, center_col, tier_idx, mask):        
+                        fp = __get_footprint (row, col, tree_size, mask_dim)
+                        if __is_footprint_clean (mask, *fp):                        
+                            __occupy_footprint (mask, points, *fp, row, col, tree_size)
 
 
             with arcpy.da.InsertCursor(matrix_fc, ['SHAPE@', 'code']) as cursor:
-                for plant_here in plant_heres:
-                    cursor.insertRow([plant_here[0], plant_here[1]])
+                for row,col in points.keys():
+                    cursor.insertRow([points[(row,col)], mask[(row,col)]])
+
 
             with arcpy.da.InsertCursor(mask_fc, ['SHAPE@', 'code', 'row', 'col', 'x', 'y', 'dim']) as cursor:
                 for r in range (0, mask_dim):
@@ -91,16 +80,29 @@ def run ():
 
 
 
-    #print ("%i points checked" % points_checked)    
-
-
-def __trim_mask (mask, mask_dim, nw_corner, polygon):
+def __trim_mask_and_get_points (nw_corner, polygon, mask):
+    points = dict()
+    mask_dim = len(mask)
     x_min, y_min, x_max, y_max = polygon.extent.XMin, polygon.extent.YMin, polygon.extent.XMax, polygon.extent.YMax, 
     for r in range (0, mask_dim):
         for c in range (0, mask_dim):
             point = __mask_to_point (r, c, nw_corner)
             if point.X < x_min or point.X > x_max or point.Y < y_min or point.Y > y_max or not point.within(polygon):
                 mask[r][c] = OUTSIDE_POLYGON
+            else:
+                mask[r][c] = VACANT
+                points[(r,c)] = point
+    return points
+
+
+
+def __get_tier_vacancies (center_row, center_col, tier_idx, mask):
+    top  = [(center_row - tier_idx, col)  for col in range (center_row - tier_idx, center_row + tier_idx + 1)]
+    right= [(row, center_col + tier_idx)  for row in range (center_col - tier_idx, center_col + tier_idx + 1)]
+    bot  = [(center_row + tier_idx, col)  for col in reversed(range (center_row - tier_idx, center_row + tier_idx + 1))]
+    left = [(row, center_col - tier_idx)  for row in reversed(range (center_col - tier_idx, center_col + tier_idx + 1))]
+    return [(row,col) for (row,col) in top + right + bot + left if mask[row, col] == VACANT]  
+
             
     
 def __mask_to_point (row, col, nw_corner):
@@ -108,32 +110,24 @@ def __mask_to_point (row, col, nw_corner):
     y = nw_corner.Y - row*diameter
     return arcpy.Point(x,y)
     
-def __is_point_clean (mask, row, col):
-    return mask[row][col] == OPEN
-
 
 def __get_footprint (row, col, tree_size, mask_dim):
-    if row == 14 and col == 14 and mask_dim == 17:
-        x=1
-        pass
-    
     fp_row_origin = max(row - tree_size,0)
     fp_col_origin = max(col - tree_size,0)
     fp_row_dim = min(2*tree_size + 1, mask_dim-fp_row_origin)
-    fp_col_dim = min(2*tree_size + 1, mask_dim-fp_col_origin)
-    
+    fp_col_dim = min(2*tree_size + 1, mask_dim-fp_col_origin)   
     return fp_row_origin, fp_col_origin, fp_row_dim, fp_col_dim
 
 
 def __is_footprint_clean (mask, fp_row, fp_col, fp_row_dim, fp_col_dim):    
     for r in range (fp_row, fp_row + fp_row_dim):
         for c in range (fp_col, fp_col + fp_col_dim):
-            if mask[r][c] != OPEN:
+            if mask[r][c] != VACANT:
                 return False
     return True
 
 
-def __occupy_footprint (mask, fp_row, fp_col, fp_row_dim, fp_col_dim, planting_row, planting_col, tree_size):
+def __occupy_footprint (mask, points, fp_row, fp_col, fp_row_dim, fp_col_dim, planting_row, planting_col, tree_size):
     for r in range (fp_row, fp_row + fp_row_dim):
         for c in range (fp_col, fp_col + fp_col_dim):
             mask[r][c] = CANOPY
