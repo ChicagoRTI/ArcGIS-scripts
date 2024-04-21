8df578a51ed9595dba5afde3eb244de804dec227
Don Morrison <DMorrison@mortonarb.org>
2024-04-21 14:43:16 -0500

    Canopy counts

--- Inventory/run.bat
+++
@@ -1,6 +0,0 @@
-echo off
-SETLOCAL EnableDelayedExpansion 
-
-SET PYTHON_EXE="C:\\Program Files\\ArcGIS\\Pro\\bin\\Python\\envs\\arcgispro-py3\\python.exe"
-
-%PYTHON_EXE%  -c "import code.merge_records; code.merge_records.run ()"
